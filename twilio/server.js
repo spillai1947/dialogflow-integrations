bfea29829c07adb1cc60d0a5973337fb466b303f
spillai1947 <42203401+spillai1947@users.noreply.github.com>
2020-09-17 08:41:03 +0100

    Update server.js

--- twilio/server.js
+++ twilio/server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //See https://dialogflow.com/docs/reference/v2-auth-setup and
 // https://cloud.google.com/dialogflow/docs/setup for details.
 
-const projectId = 'Place your dialogflow projectId here';
-const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const projectId = 'calcium-petal-288506';
+const phoneNumber = "+1 (415) 523-8886";
+const accountSid = 'AC235e99526fcb753d165ee8906010f025';
+const authToken = '0b542b517dacaed4b88b18ff84ea897b';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
